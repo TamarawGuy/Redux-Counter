ec10c252a47b29d9e6e105eab0c644ed336e5b14
Viktor <viktor.m.rumenov@gmail.com>
2022-07-01 18:11:18 +0200

    Configureed redux devtools and used useSelector for getting state

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import { store } from "./store";
+import { Provider } from "react-redux";
 
 import App from "./App";
-
 import "./index.scss";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
